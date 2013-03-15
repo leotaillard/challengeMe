a1b1ddcc5d192449002c600fca5b643be3c61806
Léo Taillard <leo.taillard@gmail.com>
2013-03-15 13:35:42 +0100

    Add title to Badge

--- challengeMe/src/java/ch/comem/models/Badge.java
+++ challengeMe/src/java/ch/comem/models/Badge.java
@@ -21,10 +21,19 @@ public class Badge implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }

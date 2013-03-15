e0213d258b49a4f3e3b0f6d98e12f87d0994999b
Léo Taillard <leo.taillard@gmail.com>
2013-03-15 13:33:47 +0100

    Add Application

---
+++ challengeMe/src/java/ch/comem/services/BadgesManager.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.comem.services;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+/**
+ *
+ * @author Leo
+ */
+@Stateless
+@WebService
+public class BadgesManager implements BadgesManagerLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
